adda92168b6c536593d8931410377aebf5b125f8
Mark <mrbruce9866@gmail.com>
2019-07-08 23:32:40 -0400

    added customer menu options

--- bamazon.js
+++ bamazon.js
@@ -2,7 +2,7 @@ var Database = require('./database');
 var inquirer = require('inquirer');
 var cTable = require('console.table');
 var products = [];
-var currentUsername;
+var currentUser;
 
 var inquireQuestions = {
     'Start': {
@@ -13,19 +13,18 @@ var inquireQuestions = {
             choices: ['Login', 'Create a New Account', 'Exit']
         }],
         run: function (answer) {
-            if(answer.loginType !== 'Exit'){
+            if (answer.loginType !== 'Exit') {
                 inquire(inquireQuestions[answer.loginType]);
-            }else{
+            } else {
                 Database.disconnectFromDatabase();
-                
-            }               
+
+            }
         }
     },
     'Create a New Account': {
-        questions: [
-            {
-            name: 'userName',
-            message: 'Create Username: ',
+        questions: [{
+                name: 'userName',
+                message: 'Create Username: ',
             },
             {
                 name: 'password1',
@@ -39,144 +38,155 @@ var inquireQuestions = {
             },
         ],
         run: function (answer) {
-            if(answer.password1 === answer.password2){
+            if (answer.password1 === answer.password2) {
                 createUser(answer.userName, answer.password1)
-            }else{
+            } else {
                 console.log("Passwords didn't match. ");
                 inquire(inquireQuestions["Create a New Account"])
             }
-            
+
         }
     },
-    'Login' :{
-        questions:[
-            {
+    'Login': {
+        questions: [{
                 name: 'name',
                 message: 'Username: '
             },
             {
                 name: 'password',
                 message: 'Password',
-                type: 'password'
+                type: 'password',
+                mask: '*'
             },
         ],
-        run: function (answer){
+        run: function (answer) {
 
-            Database.pullData(`SELECT * FROM users WHERE userName = '${answer.name}'`, 
-            function(err, res){
-                if(err) throw err;
-                if(res.length > 0){
-                    if(res[0].userPassword === answer.password){
-                        currentUsername = res[0].userName;
-                        console.log(`Logged in as ${currentUsername}`);
-                        displayProducts(function(){
-                            inquire(inquireQuestions['Customer']);
-                        });
-                        
-                    }else{
-                        console.log('Password incorrect');
-                        start();
+            Database.pullData(`SELECT * FROM users WHERE userName = '${answer.name}'`,
+                function (err, res) {
+                    if (err) throw err;
+                    if (res.length > 0) {
+                        if (res[0].userPassword === answer.password) {
+                            currentUser = res[0];
+                            console.log(`Logged in as ${currentUser.userName}`);
+                            console.log('\033c');
+                            displayProducts(function () {
+                                console.log(`Funds: $${currentUser.currentFunds.toFixed(2)}\n`)
+                                
+                                inquire(inquireQuestions['Customer']);
+                            });
+
+                        } else {
+                            console.log('Password incorrect');
+                            start();
+                        }
+                    } else {
+                        console.log(`Account for ${answer.name} has not been created!`);
+                        start();;
                     }
-                }else{
-                    console.log(`Account for ${answer.name} has not been created!`);
-                    start();;
-                }
-            })
+                })
 
         }
     },
     'Customer': {
-        questions:[
-            {
-                name:'customerOption',
-                message:'What item ID would you like to purchase? ',
-                
-            },
-        ],
-        run: function(answer){
-                var product = products.find(function(item){
-                    if(item.itemID === parseInt(answer.customerOption)){
-                        return item;
-                    }
-                });
-                console.log(product);
+        questions: [{
+            name: 'customerOption',
+            message: 'Choose an option: ',
+            type: 'list',
+            choices: ['Add Funds', 'Purchase Item', 'View Purchase History', 'Logout']
+        }, ],
+        run: function (answer) {
+            switch (answer.customerOption) {
+                case 'Add Funds':
+
+                    break;
+                case 'Purchase Item':
+
+                    break;
+                case 'View Purchase History':
+
+                    break;
+                case 'Logout':
+                    console.log(`\n ${currentUser.userName} logged out.\n`)
+                    currentUser = {};
+                    start();
+                    break;
+
+                default:
+                    break;
+            }
         }
-        
+
     },
-    'Admin':{
-        questions:[
-            {
-                name:'',
-                message:'',
-                type:'list',
-                choices:[]
-            },
-        ],
-        run: function(answer){
-                
+    'Admin': {
+        questions: [{
+            name: '',
+            message: '',
+            type: 'list',
+            choices: []
+        }, ],
+        run: function (answer) {
+
         }
-        
+
     },
-    '':{
-        questions:[
-            {
-                name:'',
-                message:'',
-                type:'list',
-                choices:[]
-            },
-        ],
-        run: function(answer){
-                
+    '': {
+        questions: [{
+            name: '',
+            message: '',
+            type: 'list',
+            choices: []
+        }, ],
+        run: function (answer) {
+
         }
-        
+
     },
-    '':{
-        questions:[
-            {
-                name:'',
-                message:'',
-                type:'list',
-                choices:[]
-            },
-        ],
-        run: function(answer){
-                
+    '': {
+        questions: [{
+            name: '',
+            message: '',
+            type: 'list',
+            choices: []
+        }, ],
+        run: function (answer) {
+
         }
-        
+
     },
 
 };
 
-Database.connectToDatabase(function (){
+Database.connectToDatabase(function () {
     start();
 });
 
-function start(){
+function start() {
+    console.log('\033c');
+    console.log("\n[***] Welcome to Bamazon [***]\n")
+
     inquire(inquireQuestions['Start']);
 }
 
 
-function createUser(name, password){
+function createUser(name, password) {
+    Database.pullData('SELECT userName FROM users', function (err, res) {
+        if (err) throw err;
 
-    Database.pullData('SELECT userName FROM users', function (err, res){
-        if(err) throw err;
-
-        if(res.find(function(result){
-            if(result.userName === name){
-                return result;
-            }
-        })){
+        if (res.find(function (result) {
+                if (result.userName === name) {
+                    return result;
+                }
+            })) {
             console.log('Username already exists');
             inquire(inquireQuestions['Create a New Account']);
-        }else{
+        } else {
 
-            Database.insertData('INSERT INTO users SET ?',{
+            Database.insertData('INSERT INTO users SET ?', {
                 userName: name,
                 userPassword: password,
-            },function(err,res){
-                if(err) throw err;
-                if(res){
+            }, function (err, res) {
+                if (err) throw err;
+                if (res) {
                     console.log(`${name} was created!`);
                     start();;
                 }
@@ -186,29 +196,24 @@ function createUser(name, password){
 
 };
 
-function displayProducts(callback){
-    Database.pullData('SELECT * FROM products', function(err, res){
-        if(err) throw err;
-        // console.log(err);
-        // console.log(res);
-        if(res.length !== 0){
+function displayProducts(callback) {
+    console.log('\033c');
+    console.log("\n[***] Bamazon Store [***]\n")
+    Database.pullData('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+        if (res.length !== 0) {
             products = res;
-            // console.log(res);
-            console.log('\n\n')
-            console.table('Products',products);
-            callback(); 
+            console.table('Products', products);
+            callback();
         }
-        
+
     });
 }
 
 
 
 function inquire(prompt) {
+    
     inquirer.prompt(prompt.questions).then(prompt.run);
 };
-
-
-
-
 
